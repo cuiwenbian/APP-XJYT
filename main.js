80b561afeb00ab83945aac8ac2527612f10309b2
15035858525 <cuiwenbian@163.com>
2019-12-09 14:07:41 +0800

    o

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ import Global from './components/global.vue' //引用文件
 Vue.config.productionTip = false;
 Vue.prototype.global_=Global;   //挂载到vue实例上面
 //Vue.prototype.url='http://192.168.1.208:8000/api/v1.1.0/'
-Vue.prototype.url='http://192.168.1.218/api/v1.1.0/'
-//Vue.prototype.url='https://t.api.ipcn.xyz/api/v1.1.0/'
+//Vue.prototype.url='http://192.168.1.218/api/v1.1.0/'
+Vue.prototype.url='https://t.api.ipcn.xyz/api/v1.1.0/'
 App.mpType = 'app'
 
 const app = new Vue({
